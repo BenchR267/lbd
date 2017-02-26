c7ef4f0234b231b351021a0ebbc0ca6a9c4f42d6
Benjamin Herzog <mail@benchr.de>
2017-02-26 08:48:44 +0100

    Create lexer in repl only once. I don't like the time.Sleep call 🤔

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,8 @@ import (
 
 	"bufio"
 
+	"time"
+
 	"github.com/BenchR267/lbd/lexer"
 )
 
@@ -15,19 +17,42 @@ var writer io.Writer = os.Stdout
 
 // Start is starting the interactive REPL (currently just printing out tokens)
 func Start() {
+
+	c := make(chan rune)
+	l := lexer.NewLexer(c)
+	l.Start()
+
 	scanner := bufio.NewScanner(reader)
 	for {
 		fmt.Fprint(writer, "lbd $ ")
 		scanner.Scan()
 		text := scanner.Text()
 		if text == "e" || text == "exit" {
+			close(c)
 			break
 		}
-		l := lexer.NewLexer(lexer.StreamFromString(text))
-		l.Start()
-		for t := range l.NextToken {
-			fmt.Fprintln(writer, t)
+
+		done := make(chan struct{})
+
+		go func() {
+			for _, r := range text {
+				c <- r
+			}
+			c <- '\n'
+			time.Sleep(time.Millisecond * 1)
+			done <- struct{}{}
+		}()
+
+	waitLoop:
+		for {
+			select {
+			case t := <-l.NextToken:
+				fmt.Fprintln(writer, t)
+			case <-done:
+				break waitLoop
+			}
 		}
+
 		fmt.Fprintln(writer)
 	}
 }
